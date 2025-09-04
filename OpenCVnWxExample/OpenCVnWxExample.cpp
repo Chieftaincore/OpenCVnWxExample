f36ea230245efd0c08dbc77ac022c12815235b30
Huseyin cht <emresutcu12@gmail.com>
2025-09-04 15:44:06 +0300

    Proje dosyası ekle.

---
+++ OpenCVnWxExample/OpenCVnWxExample.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace cv;
+
+//Deneme 1 OpenCV' kütüphaneleri çalışıyor
+int main()
+{
+    cv::Mat img = cv::imread("D:/My_OpenCV/Morph/cicek.jpg");
+    namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
+    cv::imshow("First OpenCV Application", img);
+    cv::moveWindow("First OpenCV Application", 0, 45);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+    return 0;
+}
