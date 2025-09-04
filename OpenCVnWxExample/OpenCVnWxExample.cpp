639cfabc47f4e7554e7d3d572a5241a92bb8ffd7
Huseyin cht <emresutcu12@gmail.com>
2025-09-04 17:14:31 +0300

    ENG I added Basic Example of Frontal Face Detection. | TUR Ön yüz tanımlama Basit Örneği Eklendi

--- OpenCVnWxExample/OpenCVnWxExample.cpp
+++ OpenCVnWxExample/OpenCVnWxExample.cpp
@@ -1,14 +1,22 @@
 ﻿
 
 #include <opencv2/core.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
 using namespace cv;
 
-//Deneme 1 OpenCV' kütüphaneleri çalışıyor
+int FrontalFaceDetCamera();
+int imgshow();
+
 int main()
 {
+    FrontalFaceDetCamera();
+}
+
+int imgShow() {
     cv::Mat img = cv::imread("D:/My_OpenCV/Morph/cicek.jpg");
     namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
     cv::imshow("First OpenCV Application", img);
@@ -16,4 +24,33 @@ int main()
     cv::waitKey(0);
     cv::destroyAllWindows();
     return 0;
+}
+
+
+/// <summary>
+/// BasicExample of FrontalFaceDetection
+/// </summary>
+/// <returns></returns>
+int FrontalFaceDetCamera() {
+
+    VideoCapture vid(0);
+    CascadeClassifier casd;
+    Mat matris;
+
+    casd.load("haarcascade_frontalface_default.xml");
+    cv:Vec2b yuzler;
+
+    std::vector<cv::Rect> faces;
+
+    while (true) {
+        vid.read(matris);
+        casd.detectMultiScale(matris, faces, 1.3, 5);
+
+        for (int i = 0; i < faces.size(); i++) {
+            rectangle(matris, faces[i].tl(), faces[i].br(), Scalar(50, 90, 190), 3);
+        }
+
+        imshow("Kamera", matris);
+        waitKey(25);
+    }
 }
